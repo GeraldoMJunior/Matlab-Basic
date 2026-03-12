a844ad122d1fd72e9130a834afcb80f4a71966ec
Geraldo <35813010+GeraldoMJunior@users.noreply.github.com>
2026-03-12 08:43:42 -0300

    Add files via upload
    Desenhando um segmento de reta usando a Equação Linear de Reta.

---
+++ aula10.m
@@ -0,0 +1,22 @@
+clearvars;
+clf;
+
+p1 = [1, 2];
+p2 = [9, 8];
+m = (p2(2) - p1(2)) / (p2(1) - p1(1));
+c = p1(2) - m * p1(1);
+
+x = 1:0.1:9;
+y = zeros(size(x));
+
+for i = 1:length(x)
+    y(i) = m * x(i) + c;
+end
+
+hold on;
+plot(x, y, 'o', 'Color', 'b', 'MarkerSize', 3);
+plot([p1(1) p2(1)], [p1(2) p2(2)], 'ro', 'MarkerSize', 6, 'MarkerFaceColor','r');
+grid on;
+axis([0 10 0 10]);
+title(['Line: y = ' num2str(m) 'x + ' num2str(c)]);
+hold off;
